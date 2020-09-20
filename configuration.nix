984d6b60aa0c5337cd8d46396824469955cb8c56
ajpen <anferneejervis@gmail.com>
2020-09-20 10:42:07 -0400

    made server status manual

--- configuration.nix
+++ configuration.nix
@@ -4,7 +4,6 @@
   imports = [
 	./environments/base.nix
 	./environments/graphical.nix
-	./environments/server.nix    # this laptop runs all the time. Uncomment to disable
 	./environments/networking.nix
   ];
 
